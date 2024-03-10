ddf6b920342b5b726225210c53379121cc39ef97
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-03-10 11:34:01 +0530

    Registered routes and started server

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/charmbracelet/log"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+
+	"devstream.in/blogs/auth"
+	"devstream.in/blogs/config"
+	"devstream.in/blogs/controllers"
+	"devstream.in/blogs/middlewares"
+	"devstream.in/blogs/repositories"
+)
+
+func main() {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("could not load config", "err", err)
+	}
+	log.Info(conf.DatabaseConf.Source)
+	repositories.Db, err = sql.Open("postgres", conf.DatabaseConf.Source)
+	if err != nil {
+		log.Fatal("Failed to connect to database.")
+	}
+
+	defer repositories.Db.Close()
+
+	r := mux.NewRouter()
+	r.Use(middlewares.WithCorsMiddleware)
+
+	r.HandleFunc("/posts", controllers.ReturnAllPosts).Methods(http.MethodGet)
+	r.HandleFunc("/posts/batch/{id}", controllers.ReturnBatchPosts).Methods(http.MethodGet)
+	r.HandleFunc("/posts/{id}", controllers.ReturnSinglePost).Methods(http.MethodGet)
+
+	// r.HandleFunc("/{username}", controllers.ReturnUserData).Methods(http.MethodGet)
+	// r.HandleFunc("/{userid}/posts", controllers.ReturnUserPosts).Methods(http.MethodGet)
+
+	restrictedR := r.Path("/").Subrouter()
+	restrictedR.Use(middlewares.WithAuthentication)
+
+	restrictedR.HandleFunc("/posts/{id}", controllers.UpdatePost).Methods(http.MethodOptions, http.MethodPut)
+	restrictedR.HandleFunc("/posts/{id}", controllers.DeletePost).Methods(http.MethodOptions, http.MethodDelete)
+	restrictedR.HandleFunc("/post", controllers.CreateNewPost).Methods(http.MethodOptions, http.MethodPost)
+
+	authR := r.PathPrefix("/auth").Subrouter()
+	authR.HandleFunc("/signup", auth.SignUp).Methods(http.MethodOptions, http.MethodPost)
+	authR.HandleFunc("/login", auth.LogIn).Methods(http.MethodOptions, http.MethodPost)
+	authR.HandleFunc("/refresh", auth.Refresh).Methods(http.MethodOptions, http.MethodPost)
+
+	// fs := http.FileServer(http.Dir("./frontend/dist"))
+	// r.PathPrefix("/").Handler(fs)
+
+	err = http.ListenAndServe(":"+fmt.Sprint(conf.Port), r)
+	if err != nil {
+		log.Fatal("could not start http server", "err", err)
+	}
+}
