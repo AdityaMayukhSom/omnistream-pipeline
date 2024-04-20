502ba5e80e7953bf5e8e391996066e0b0c7ff8a7
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-04-20 15:38:29 +0530

    ADD: handler function for hello world template

---
+++ api/controllers/templates/helloworld.go
@@ -0,0 +1,14 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func HelloWorld(c echo.Context) error {
+	return c.Render(http.StatusOK, "helloworld.html", map[string]interface{}{
+		"message": "Hello from Rendered Views",
+	})
+
+}
