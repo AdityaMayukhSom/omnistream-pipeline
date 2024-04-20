4d30d8c2934363013771f2eaf4361b2c8ec50a30
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-04-20 17:50:16 +0530

    ADD: migrate function and sql query logging

--- database/postgres/postgres_database.go
+++ database/postgres/postgres_database.go
@@ -1,25 +1,70 @@
 package postgresql
 
-import "devstream.in/pixelated-pipeline/services/models"
+import (
+	"database/sql"
+	goDefaultLog "log"
+	"os"
+	"time"
+
+	"devstream.in/pixelated-pipeline/config"
+	"devstream.in/pixelated-pipeline/database/entities"
+	"devstream.in/pixelated-pipeline/services/models"
+
+	"github.com/charmbracelet/log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
 
 type PostgresDatabase struct {
+	gormDB *gorm.DB
 }
 
-func (psql *PostgresDatabase) SetupDatabase() {
-	// Db, err := sql.Open("pgx", config.DefaultConfig.DatabaseConf.Source)
-	// if err != nil {
-	// 	log.Fatal("Failed to connect to database.")
-	// }
+func NewPostgresDatabase() *PostgresDatabase {
+	sqlDB, err := sql.Open("pgx", config.GetDataSourceUri())
+	if err != nil {
+		log.Fatal("Failed to connect to database.")
+	}
 
-	// gormDB, err := gorm.Open(postgres.New(postgres.Config{
-	// 	Conn: Db,
-	// }), &gorm.Config{})
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{
+		Logger: logger.New(
+			goDefaultLog.New(os.Stdout, "\r\n", goDefaultLog.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold:             time.Second, // Slow SQL threshold
+				LogLevel:                  logger.Info, // Log level
+				IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+				ParameterizedQueries:      true,        // Don't include params in the SQL log
+				Colorful:                  true,        // Disable color
+			},
+		),
+	})
 
-	// Database = gormDB
+	if err != nil {
+		log.Fatal("Failed to connect to database.")
+	}
+
+	database := &PostgresDatabase{
+		gormDB: gormDB,
+	}
+
+	return database
 }
 
-func (psql *PostgresDatabase) FindUserByUsername(username string) {}
-func (psql *PostgresDatabase) FindUserByEmail(email string)       {}
+func (psql *PostgresDatabase) Migrate() {
+	psql.gormDB.AutoMigrate(
+		&entities.User{},
+		&entities.Post{},
+		&entities.Token{},
+	)
+}
+
+func (psql *PostgresDatabase) FindUserByUsername(username string) {
+
+}
+func (psql *PostgresDatabase) FindUserByEmail(email string) {}
 
 func (psql *PostgresDatabase) DeleteUserByUsername(username string) {}
 func (psql *PostgresDatabase) DeleteUserByEmail(email string)       {}
@@ -35,12 +80,12 @@ func (psql *PostgresDatabase) DeletePostById(id string) {}
 func (psql *PostgresDatabase) CreatePost(post models.Post) {}
 func (psql *PostgresDatabase) UpdatePost(post models.Post) {}
 
-func (psql *PostgresDatabase) CleanupDatabase() {
-	// Db, err := Database.DB()
-	// if err != nil {
-	// 	// do something useful
-	// }
+func (psql *PostgresDatabase) Close() {
+	Db, err := psql.gormDB.DB()
+	if err != nil {
+		// do something useful
+	}
 
-	// Db.Close()
+	Db.Close()
 
 }
