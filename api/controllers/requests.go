6b4f3fd16ae9ca1be6f8354cdac681bb8932e812
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-04-20 23:32:25 +0530

    ADD: tags for json and formdata binding in echo

--- api/controllers/requests.go
+++ api/controllers/requests.go
@@ -4,10 +4,15 @@ type RequestCreatePost struct {
 }
 
 type RequestRegisterUser struct {
-	Name     string
-	Email    string
-	Username string
-	Password string
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Username string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
+}
+
+type RequestLoginUser struct {
+	Username string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
 }
 
 type RequestUpdateUser struct {
