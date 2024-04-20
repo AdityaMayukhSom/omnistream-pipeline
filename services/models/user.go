b78ac3c367e5646f8188c8cd929781b0945ace85
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-04-20 23:33:26 +0530

    ADD: model structs for data passing between layers

--- services/models/user.go
+++ services/models/user.go
@@ -1,4 +1,13 @@
 package models
 
 type User struct {
+	Username string
+	Password string
+	Name     string
+	Email    string
+}
+
+type LoginCredential struct {
+	Username string
+	Password string
 }
