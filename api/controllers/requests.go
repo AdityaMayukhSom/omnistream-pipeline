103351b37f2a38b766d78d744e55c827d3449d8c
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-04-20 18:02:11 +0530

    FIX: removed packages for requests and responses
    - created structs for individual scenarios

---
+++ api/controllers/requests.go
@@ -0,0 +1,13 @@
+package controllers
+
+type RequestCreatePost struct {
+}
+
+type RequestRegisterUser struct {
+}
+
+type RequestUpdateUser struct {
+}
+
+type RequestUpdatePost struct {
+}
---
+++ api/controllers/responses.go
@@ -0,0 +1,22 @@
+package controllers
+
+type ResponseCreatedPost struct {
+}
+
+type ResponseGetMultiplePosts struct {
+}
+
+type ResponseGetMultipleUsers struct {
+}
+
+type ResponseGetSinglePost struct {
+}
+
+type ResponseGetSingleUser struct {
+}
+
+type ResponseUpdatedPost struct {
+}
+
+type ResponseUpdatedUser struct {
+}
