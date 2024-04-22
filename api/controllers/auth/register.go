6ee04065a9441f6bd7892ab74f1287de8972354f
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-04-22 20:48:56 +0530

    FIX: controller function for user registration

--- api/controllers/auth/register.go
+++ api/controllers/auth/register.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 
 	"devstream.in/pixelated-pipeline/api/controllers"
+	service "devstream.in/pixelated-pipeline/services"
+	"devstream.in/pixelated-pipeline/services/models"
 	"github.com/labstack/echo/v4"
 )
 
-func SignUp(c echo.Context) error {
+func SignUp(c echo.Context) (err error) {
 	var req controllers.RequestRegisterUser
 
-	err := c.Bind(&req)
+	err = c.Bind(&req)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, controllers.ResponseError{
 			ErrorMessage: "could not parse request body",
@@ -19,6 +21,21 @@ func SignUp(c echo.Context) error {
 	}
 
 	fmt.Println(req)
+
+	userService := service.NewUserService()
+
+	err = userService.RegisterUser(models.User{
+		Username: req.Username,
+		Name:     req.Name,
+		Password: req.Password,
+		Email:    req.Email,
+	})
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, controllers.ResponseError{
+			ErrorMessage: err.Error(),
+		})
+	}
 
 	return c.JSON(http.StatusCreated, map[string]any{
 		"message": "user successfully registered",
