dd2d4f7c1a24985207d78b1b9dd86688232e1719
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-04 21:38:33 +0530

    FIX: extracted routes to individual functions

--- api/router.go
+++ api/router.go
@@ -2,16 +2,15 @@ package api
 
 import (
 	"fmt"
+	"os"
 
 	"devstream.in/pixelated-pipeline/api/controllers"
 	"devstream.in/pixelated-pipeline/config"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-
-	echoSwagger "github.com/swaggo/echo-swagger"
-
-	_ "github.com/swaggo/echo-swagger/example/docs"
+	// echoSwagger "github.com/swaggo/echo-swagger"
+	// _ "github.com/swaggo/echo-swagger/example/docs"
 )
 
 type Router interface {
@@ -23,7 +22,7 @@ func NewRouter() Router {
 	return NewEchoRouter()
 }
 
-// ------------------------------------------------------------------------
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 type EchoRouter struct {
 	echo *echo.Echo
@@ -46,32 +45,66 @@ func (er *EchoRouter) RegisterRoutes() {
 		},
 	}))
 
-	er.echo.Static("/", "static")
+	// To declare global router wise static directory.
+	// er.echo.Static("/", "static")
 
-	er.echo.GET("/swagger/*", echoSwagger.WrapHandler)
+	// For documentation generation for API, currently not required
+	// er.echo.GET("/swagger/*", echoSwagger.WrapHandler)
 
+	er.registerWebRoutes()
+	er.registerApiRoutes()
+}
+
+func (er *EchoRouter) registerWebRoutes() {
 	er.echo.Renderer = NewRenderer("./views/*", true)
-	er.echo.GET("/hello-world", controllers.HelloWorld)
 
+	// Group made out from the same path as the base echo router
+	fileRoutes := er.echo.Group("")
+
+	// For serving static resources such as images, javascript, css etc
+	// The static directory is respect to the root directory of the application.
+	// The separate file systems are for fine grained control so that not any other
+	// file stored inside the static directory can be directly accessed.
+	tmplJsFS := os.DirFS("static/js")
+	tmplCssFS := os.DirFS("static/css")
+	tmplImgFS := os.DirFS("static/img")
+
+	// setting up to serve static files
+	fileRoutes.StaticFS("/js", tmplJsFS)
+	fileRoutes.StaticFS("/css", tmplCssFS)
+	fileRoutes.StaticFS("/img", tmplImgFS)
+
+	// Setting up the URLs to serve user facing UIs.
+	// Routes in which static HTML is served.
+	fileRoutes.GET("/login", controllers.DisplayLoginPage)
+	fileRoutes.GET("/signup", controllers.DisplaySignupPage)
+
+	// Routes which internally renders the HTML from templates.
+	fileRoutes.GET("/helloworld", controllers.RenderHelloWorldPage)
+
+}
+
+// Registers routes concerned with the API endpoints.
+func (er *EchoRouter) registerApiRoutes() {
 	apiV1 := er.echo.Group("/api/v1")
 
 	apiV1.GET("/posts", controllers.ReturnAllPosts)
 	apiV1.GET("/posts/:username/:postname", controllers.ReturnSinglePost)
 
-	authRoute := apiV1.Group("/auth")
-	authRoute.POST("/register", controllers.SignUp)
-	authRoute.POST("/login", controllers.LogIn)
-	authRoute.POST("/refresh", controllers.Refresh)
-	authRoute.GET("/logout", controllers.LogOut)
+	authApiRoute := apiV1.Group("/auth")
+	authApiRoute.POST("/register", controllers.SignUp)
+	authApiRoute.POST("/login", controllers.LogIn)
+	authApiRoute.POST("/refresh", controllers.Refresh)
+	authApiRoute.GET("/logout", controllers.LogOut)
 
-	restrictedRoute := apiV1.Group("/")
-	restrictedRoute.Use(echojwt.WithConfig(echojwt.Config{
+	restrictedApiRoute := apiV1.Group("/")
+	restrictedApiRoute.Use(echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte("secret"),
 	}))
 
-	restrictedRoute.POST("/posts", controllers.CreatePost)
-	restrictedRoute.DELETE("/posts/{id}", controllers.DeletePost)
-	restrictedRoute.PUT("/posts/{id}", controllers.UpdatePost)
+	restrictedApiRoute.POST("/posts", controllers.CreatePost)
+	restrictedApiRoute.DELETE("/posts/{id}", controllers.DeletePost)
+	restrictedApiRoute.PUT("/posts/{id}", controllers.UpdatePost)
 }
 
 func (er *EchoRouter) Start() {
