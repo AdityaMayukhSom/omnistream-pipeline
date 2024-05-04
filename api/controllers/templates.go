12b82bbf289f1b0da545270e33284343dbad6e79
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-04 21:50:10 +0530

    REFACTOR: Renderer alongside templates

--- api/controllers/templates.go
+++ api/controllers/templates.go
@@ -1,10 +1,40 @@
 package controllers
 
 import (
+	"io"
 	"net/http"
+	"text/template"
 
 	"github.com/labstack/echo/v4"
 )
+
+// A structure implementing the echo.Rendered interface for templates rendering.
+type Renderer struct {
+	template *template.Template
+	debug    bool
+	location string
+}
+
+func (t *Renderer) ReloadTemplates() {
+	t.template = template.Must(template.ParseGlob(t.location))
+}
+
+func (t *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	if t.debug {
+		t.ReloadTemplates()
+	}
+
+	return t.template.ExecuteTemplate(w, name, data)
+}
+
+func NewRenderer(location string, debug bool) *Renderer {
+	tp := &Renderer{
+		location: location,
+		debug:    debug,
+	}
+	tp.ReloadTemplates()
+	return tp
+}
 
 func RenderHelloWorldPage(c echo.Context) error {
 	return c.Render(http.StatusOK, "helloworld.go.html", map[string]interface{}{
