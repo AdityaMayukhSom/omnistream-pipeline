36f51bcc5b2d9c69758604529d84d15ebc32ca3c
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-05 18:40:37 +0530

    ADD: api constants

--- api/constants/constants.go
+++ api/constants/constants.go
@@ -4,3 +4,13 @@ const (
 	CookieNameAccessToken  = "x-access-token-pixelated-pipeline"
 	CookieNameRefreshToken = "x-refresh-token-pixelated-pipeline"
 )
+
+const (
+	DefaultAuthenticatedRoute   = "/home"
+	DefaultUnauthenticatedRoute = "/login"
+)
+
+const (
+	ContextAttributeKeyUsername = "username"
+	ContextAttributeKeyName     = "name"
+)
