48c27790469a2045590fd2b40bf881970ae38c6f
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-05 18:40:46 +0530

    ADD: service constants

--- services/constants/constants.go
+++ services/constants/constants.go
@@ -1,8 +1,17 @@
 package serviceConstant
 
 const (
-	ClaimKeyName       = "user_name"
-	ClaimKeyUsername   = "user_username"
-	ClaimKeyEmail      = "user_email"
-	ClaimKeyExpiration = "token_expiration"
+	ClaimKeyName        = "user_name"
+	ClaimKeyUsername    = "user_username"
+	ClaimKeyEmail       = "user_email"
+	ClaimKeySubject     = "sub" // Subject (user identifier)
+	ClaimKeyIssuer      = "iss" // Issuer
+	ClaimKeyAudience    = "aud" // Audience (user role)
+	ClaimKeyExpiration  = "exp" // Expiration time
+	ClaimKeyIssuedAt    = "iat" // Issued at
+	ClaimKeyRefreshUUID = "refresh_uuid"
+	ClaimKeyAccessUUID  = "access_uuid"
+	ClaimKeyAuthorized  = "authorized"
+
+	ClaimValueIssuer = "pixelated-project"
 )
