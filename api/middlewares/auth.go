bdf473623b6350a5c37568be8e938edb22ad10ca
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-06 11:24:13 +0530

    FIX: headers to discourage browser caching

--- api/middlewares/auth.go
+++ api/middlewares/auth.go
@@ -2,11 +2,13 @@ package middlewares
 
 import (
 	"net/http"
+	"time"
 
 	apiConstant "devstream.in/pixelated-pipeline/api/constants"
 	"devstream.in/pixelated-pipeline/api/dto"
 	"devstream.in/pixelated-pipeline/config"
 	"devstream.in/pixelated-pipeline/services"
+	"github.com/go-http-utils/headers"
 	"github.com/labstack/echo/v4"
 )
 
@@ -20,10 +22,15 @@ func WithAuthentication(next echo.HandlerFunc) echo.HandlerFunc {
 			})
 		}
 
+		c.Response().Header().Add(headers.CacheControl, "private, no-store, max-age=0, no-cache, must-revalidate, post-check=0, pre-check=0")
+		c.Response().Header().Add(headers.Expires, time.Unix(0, 0).Format(http.TimeFormat))
+		c.Response().Header().Add(headers.Pragma, "no-cache")
+
 		if tokenCookie, err := c.Cookie(apiConstant.CookieNameAccessToken); err == nil {
 			tokenService := services.NewTokenService()
-			if username, err := tokenService.ValidateToken(tokenCookie.Value, config.GetAccessSecretKey()); err == nil {
+			if username, name, err := tokenService.ValidateToken(tokenCookie.Value, config.GetAccessSecretKey()); err == nil {
 				c.SetCookie(tokenCookie)
+				c.Set(apiConstant.ContextAttributeKeyName, name)
 				c.Set(apiConstant.ContextAttributeKeyUsername, username)
 				return next(c)
 			}
@@ -39,14 +46,20 @@ func WithAuthentication(next echo.HandlerFunc) echo.HandlerFunc {
 func WithAlreadyAuthenticated(next echo.HandlerFunc) echo.HandlerFunc {
 	// log.Info("with already authenticated hit")
 	return func(c echo.Context) error {
+		c.Response().Header().Add(headers.CacheControl, "private, no-store, max-age=0, no-cache, must-revalidate, post-check=0, pre-check=0")
+		c.Response().Header().Add(headers.Expires, time.Unix(0, 0).Format(http.TimeFormat))
+		c.Response().Header().Add(headers.Pragma, "no-cache")
+
 		if tokenCookie, err := c.Cookie(apiConstant.CookieNameAccessToken); err == nil {
 			tokenService := services.NewTokenService()
-			if username, err := tokenService.ValidateToken(tokenCookie.Value, config.GetAccessSecretKey()); err == nil {
+			if username, name, err := tokenService.ValidateToken(tokenCookie.Value, config.GetAccessSecretKey()); err == nil {
 				c.SetCookie(tokenCookie)
+				c.Set(apiConstant.ContextAttributeKeyName, name)
 				c.Set(apiConstant.ContextAttributeKeyUsername, username)
 				return c.Redirect(http.StatusSeeOther, apiConstant.DefaultAuthenticatedRoute)
 			}
 		}
+
 		return next(c)
 	}
 }
