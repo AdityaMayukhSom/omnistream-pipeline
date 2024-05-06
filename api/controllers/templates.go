0e30be1b2368fc25fa5bc5493bb82a485e094e49
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-06 11:23:56 +0530

    FIX: pass username and name to all templates, use type alias for map[string]any

--- api/controllers/templates.go
+++ api/controllers/templates.go
@@ -1,47 +1,33 @@
 package controllers
 
 import (
-	"io"
 	"net/http"
-	"text/template"
 
+	apiConstant "devstream.in/pixelated-pipeline/api/constants"
 	"github.com/labstack/echo/v4"
 )
 
-// A structure implementing the echo.Rendered interface for templates rendering.
-type Renderer struct {
-	template *template.Template
-	debug    bool
-	location string
-}
-
-func (t *Renderer) ReloadTemplates() {
-	t.template = template.Must(template.ParseGlob(t.location))
-}
-
-func (t *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	if t.debug {
-		t.ReloadTemplates()
-	}
-
-	return t.template.ExecuteTemplate(w, name, data)
-}
-
-func NewRenderer(location string, debug bool) *Renderer {
-	tp := &Renderer{
-		location: location,
-		debug:    debug,
-	}
-	tp.ReloadTemplates()
-	return tp
-}
+// Inspired by gin.H
+type H map[string]any
 
 func RenderHelloWorldPage(c echo.Context) error {
-	return c.Render(http.StatusOK, "helloworld.go.html", map[string]interface{}{
-		"message": "Hello from Rendered Views",
+	return c.Render(http.StatusOK, "helloworld", H{
+		"name":     c.Get(apiConstant.ContextAttributeKeyName),
+		"username": c.Get(apiConstant.ContextAttributeKeyUsername),
+		"message":  "Hello, Sweetheart <3",
 	})
 }
 
 func RenderHomePage(c echo.Context) error {
-	return c.Render(http.StatusOK, "homepage.go.html", map[string]interface{}{})
+	return c.Render(http.StatusOK, "home", H{
+		"name":     c.Get(apiConstant.ContextAttributeKeyName),
+		"username": c.Get(apiConstant.ContextAttributeKeyUsername),
+	})
+}
+
+func RenderWritePage(c echo.Context) error {
+	return c.Render(http.StatusOK, "write", H{
+		"name":     c.Get(apiConstant.ContextAttributeKeyName),
+		"username": c.Get(apiConstant.ContextAttributeKeyUsername),
+	})
 }
