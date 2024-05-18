4229c14baf9ff50771affe742dfc82623e99caab
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-19 00:16:47 +0530

    FIX: authorization wrong cookie setup in non authenticated websites

--- api/middlewares/auth.go
+++ api/middlewares/auth.go
@@ -14,14 +14,6 @@ import (
 
 func WithAuthentication(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		// log.Info("auth middleware hit")
-
-		if c.Request().Method == http.MethodOptions {
-			return c.JSON(http.StatusMethodNotAllowed, dto.ResponseError{
-				ErrorMessage: "http method options not supported",
-			})
-		}
-
 		c.Response().Header().Add(headers.CacheControl, "private, no-store, max-age=0, no-cache, must-revalidate, post-check=0, pre-check=0")
 		c.Response().Header().Add(headers.Expires, time.Unix(0, 0).Format(http.TimeFormat))
 		c.Response().Header().Add(headers.Pragma, "no-cache")
@@ -29,34 +21,32 @@ func WithAuthentication(next echo.HandlerFunc) echo.HandlerFunc {
 		if tokenCookie, err := c.Cookie(apiConstant.CookieNameAccessToken); err == nil {
 			tokenService := services.NewTokenService()
 			if username, name, err := tokenService.ValidateToken(tokenCookie.Value, config.GetAccessSecretKey()); err == nil {
-				c.SetCookie(tokenCookie)
 				c.Set(apiConstant.ContextAttributeKeyName, name)
 				c.Set(apiConstant.ContextAttributeKeyUsername, username)
 				return next(c)
 			}
 		}
 
-		// return c.JSON(http.StatusUnauthorized, dto.ResponseError{
-		// 	ErrorMessage: "please login to continue",
-		// })
 		return c.Redirect(http.StatusSeeOther, apiConstant.DefaultUnauthenticatedRoute)
 	}
 }
 
 func WithAlreadyAuthenticated(next echo.HandlerFunc) echo.HandlerFunc {
-	// log.Info("with already authenticated hit")
 	return func(c echo.Context) error {
 		c.Response().Header().Add(headers.CacheControl, "private, no-store, max-age=0, no-cache, must-revalidate, post-check=0, pre-check=0")
 		c.Response().Header().Add(headers.Expires, time.Unix(0, 0).Format(http.TimeFormat))
 		c.Response().Header().Add(headers.Pragma, "no-cache")
 
-		if tokenCookie, err := c.Cookie(apiConstant.CookieNameAccessToken); err == nil {
+		if tokenCookie, err := c.Cookie(apiConstant.CookieNameAccessToken); err == nil && tokenCookie.Valid() == nil {
 			tokenService := services.NewTokenService()
 			if username, name, err := tokenService.ValidateToken(tokenCookie.Value, config.GetAccessSecretKey()); err == nil {
-				c.SetCookie(tokenCookie)
-				c.Set(apiConstant.ContextAttributeKeyName, name)
-				c.Set(apiConstant.ContextAttributeKeyUsername, username)
-				return c.Redirect(http.StatusSeeOther, apiConstant.DefaultAuthenticatedRoute)
+				return c.JSON(http.StatusOK, dto.ResponseLoginUser{
+					Type: apiConstant.RESPONSE_TYPE_ALREADY_AUTHENTICATED,
+					User: dto.UserDTO{
+						Name:     name,
+						Username: username,
+					},
+				})
 			}
 		}
 
