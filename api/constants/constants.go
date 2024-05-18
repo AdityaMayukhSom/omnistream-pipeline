34a972f13d6112aea8f8d90f059be51491a506de
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-19 00:18:16 +0530

    ADD: token cookie path constants

--- api/constants/constants.go
+++ api/constants/constants.go
@@ -6,6 +6,11 @@ const (
 )
 
 const (
+	CookiePathAccessToken  = "/"
+	CookiePathRefreshToken = "/api/v1/auth/"
+)
+
+const (
 	DefaultAuthenticatedRoute   = "/home"
 	DefaultUnauthenticatedRoute = "/login"
 )
@@ -13,4 +18,16 @@ const (
 const (
 	ContextAttributeKeyUsername = "username"
 	ContextAttributeKeyName     = "name"
+)
+
+const (
+	RESPONSE_TYPE_ALREADY_AUTHENTICATED string = "already_authenticated"
+	RESPOSNE_TYPE_NEWLY_AUTHENTICATED   string = "newly_authenticated"
+	RESPONSE_TYPE_SUCCESSFUL_LOGOUT     string = "logged_out_successfully"
+	RESPONSE_TYPE_SUCCESSFUL_REGISTERED string = "user_successfully_registered"
+)
+
+const (
+	ERROR_TYPE_INVALID_BODY              string = "could_not_parse_request_body"
+	ERROR_TYPE_INVALID_LOGIN_CREDENTIALS string = "invalid_login_credentials"
 )
