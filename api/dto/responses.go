d545dd3455656dad56733990d19dcae28ad131c3
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-19 00:17:28 +0530

    ADD: login, logout and register user response structure

--- api/dto/responses.go
+++ api/dto/responses.go
@@ -1,6 +1,24 @@
 package dto
 
+import "time"
+
+type ResponseLoginUser struct {
+	Type string  `json:"type"`
+	User UserDTO `json:"user"`
+}
+
+type ResponseLogoutUser struct {
+	Type       string    `json:"type"`
+	LogoutTime time.Time `json:"logoutTime"`
+}
+
 type ResponseRegisteredUser struct {
+	Type string `json:"type"`
+}
+
+type ResponseError struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
 }
 
 type ResponseCreatedPost struct {
@@ -22,8 +40,4 @@ type ResponseUpdatedPost struct {
 }
 
 type ResponseUpdatedUser struct {
-}
-
-type ResponseError struct {
-	ErrorMessage string
 }
