125b40c917fd9d1dc6d9825a890431e3993393af
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-05-23 13:37:55 +0530

    remove: boilerplate commented code

--- api/controllers/user.go
+++ api/controllers/user.go
@@ -37,27 +37,6 @@ func EditUserData(c echo.Context) error {
 }
 
 func ReturnUserData(c echo.Context) error {
-	// fmt.Println("returning user data...")
-	// vars := mux.Vars(r)
-	// username := vars["username"]
-
-	// user, err := repositories.RetrieveUserByUsername(username)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	enc, _ := json.Marshal(map[string]any{
-	// 		"message": err.Error(),
-	// 		"user":    nil,
-	// 	})
-	// 	w.Write(enc)
-	// 	return
-	// }
-	// user.Password = "" // do not send user password encrypted value
-	// w.WriteHeader(http.StatusOK)
-	// enc, _ := json.Marshal(map[string]any{
-	// 	"message": "successfully retrieved user",
-	// 	"user":    user,
-	// })
-	// w.Write(enc)
 
 	return nil
 }
